3998ee8727dda4fb380d2796fc9341ac682ab399
Sai Kumar Gummadi <skgummadi@operative.com>
2017-07-19 20:25:39 -0400

    Intial commit
    Spring Discovery config server setup

---
+++ src/main/java/com/spring/practice/DiscoveryApplication.java
@@ -0,0 +1,19 @@
+package com.spring.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryApplication 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(DiscoveryApplication.class, args);
+    }
+}
